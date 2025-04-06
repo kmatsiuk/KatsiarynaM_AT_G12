b6f5b6f0244e5404a7ff3b4c3ced246ff5863040
kmatsiuk <katia.mtk404@gmail.com>
2025-04-06 21:34:49 +0300

    Task 2 renew

--- src/homework/day4/playground/essence/creatures/Beetle.java
+++ src/homework/day4/playground/essence/creatures/Beetle.java
@@ -4,7 +4,7 @@ public abstract class Beetle extends Insect {
 
     public void nest (Carrot home) {
         if (this.getMass() < home.getMass()) {
-            int odds = this.getMass() / home.getMass(); //this потом
+            int odds = this.getMass() / home.getMass(); //this почему
             System.out.printf("I am %s and I will nest there with %d my family members!", name, odds);
         } else {
             System.out.println("This carrot is too small for nesting :(");
