8d412fa9d5b8af314f227b6cd5b8dbaa315b25d5
kmatsiuk <katia.mtk404@gmail.com>
2025-04-07 00:45:07 +0300

    Task 4 durdom na kolesah

--- src/homework/day4/playground/essence/creatures/Fly.java
+++ src/homework/day4/playground/essence/creatures/Fly.java
@@ -2,4 +2,7 @@ package homework.day4.playground.essence.creatures;
 import homework.day4.playground.essence.Flyable;
 
 public class Fly extends Insect implements Flyable {
+    public void fly(String direction) {
+        System.out.printf("I am HELP my name is %s and I am flying to %s", this.getClass().getSimpleName(), direction);
+    }
 }
