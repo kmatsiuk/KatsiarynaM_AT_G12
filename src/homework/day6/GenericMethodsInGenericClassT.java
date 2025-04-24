cd003e44f63fd696d9c695a1ae0bb588ad84f52b
kmatsiuk <katia.mtk404@gmail.com>
2025-04-25 00:06:09 +0300

    Gen task 1-2

---
+++ src/homework/day6/GenericMethodsInGenericClassT.java
@@ -0,0 +1,14 @@
+package homework.day6;
+
+public class GenericMethodsInGenericClassT<T> {
+    void genericMethodOneGenArg(T t) { //может быть любая буква?
+        System.out.printf("I am an object of %s class\n", t.getClass().getSimpleName()); //%s не правильно? типа я еще не знаю сама это стринг или нет?
+    }
+    public String genericMethodTwoGenArgs(T a, T b) {
+        return String.format("We are objects of %s class and %s class\n", a.getClass().getSimpleName(), b.getClass().getSimpleName());
+    }
+    void genericMethodHalfGenArgs(T c, String line) {
+        int num = line.length();
+        System.out.printf("I got an object of %s class and string with %d characters\n", c.getClass().getSimpleName(), num);
+    }
+}
