ac32eb061ae348856eac4cb2e0f26d6955aad690
kmatsiuk <katia.mtk404@gmail.com>
2025-05-16 23:07:23 +0300

    maven

--- src/main/java/homework/day9/RunnerFigures.java
+++ src/main/java/homework/day9/RunnerFigures.java
@@ -1,16 +1,13 @@
 package homework.day9;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 public class RunnerFigures {
     public static void main(String[] args) {
-        List<String> figures = Stream.of("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс")
-                .toList();
-
+        Stream.of("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс")
+                .map(x -> x.length()).filter(x -> x > 4).forEach(System.out::println);
         // Заменить каждое слово на целое число, соответствующее количеству букв в слове
         // Отфильтровать числа, которые больше 4
         // Отпечатать все оставшиеся числа в консоль
-        System.out.println(figures.stream().map(x -> x.length()).filter(x -> x > 4).toList());
     }
 }
